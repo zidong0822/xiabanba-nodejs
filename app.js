37d21f320aed5ffc4f67daf7ae5d1fa1c6ffd58f
lvwenhao <lvwenhao@huohua.cn>
2021-12-28 11:37:22 +0800

    修改文件

--- app.js
+++ app.js
@@ -16,12 +16,19 @@
 
 // [START gae_node_request_example]
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-app.use('/shrink', createProxyMiddleware({ target: 'https://api.tinify.com', changeOrigin: true }));
-app.listen(3000);
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world!').end();
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
 // [END gae_node_request_example]
 
 module.exports = app;
