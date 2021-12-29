4f145116559683d4ee36d39b01dc1a78b906fbff
侯强强 <houqiangqiang@houqiangqiangdeMacBook-Pro.local>
2021-12-29 23:28:04 +0800

    cors

--- app.js
+++ app.js
@@ -16,25 +16,12 @@
 
 // [START gae_node_request_example]
 const express = require('express');
+const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-//设置跨域访问
-app.all("*", function (req, res, next) {
-  //设置允许跨域的域名，*代表允许任意域名跨域
-  res.header("Access-Control-Allow-Origin", req.headers.origin || '*');
-  // //允许的header类型
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-  // //跨域允许的请求方式 
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-  // 可以带cookies
-  res.header("Access-Control-Allow-Credentials", true);
-  if (req.method == 'OPTIONS') {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-})
+
+app.use(cors()); 
 app.use('/shrink', createProxyMiddleware({ target: 'https://api.tinify.com', changeOrigin: true,onProxyReq:(proxyReq)=>{
     proxyReq.setHeader('authorization','Basic YXBpOlRQUmg0RlpRWkhQTmpOUW5WTlhYWjNjSnh5eWJGVGgy');
     proxyReq.setHeader('Content-type','application/json');
