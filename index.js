7fc505a63f3c39c414b2030fc6bd5453c3eb5394
lvwenhao <lvwenhao@huohua.cn>
2021-12-30 17:45:46 +0800

    测试API

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
-const express = require("express");
-const app = express();
+// Copyright 2017 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-app.all("*",function(req,res,next){
-  res.header("Access-Control-Allow-Origin","*");
-  res.header("Access-Control-Allow-Headers","content-type");
-  res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
-  if (req.method.toLowerCase() == 'options')
-      res.send(200);
-  else
-      next();
-})
+'use strict';
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
-
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+const express = require('express')
+const cors = require('cors')
+const axios = require('axios');
+const app = express()
+const bodyParser = require('body-parser')
+const port = 8080
+app.use(cors());
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Headers', "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+app.use(bodyParser.raw({ type:'*/*' }));
+app.post('/', (req, res) => {
+axios({
+    method: 'POST',
+    url: `https://api.tinify.com/shrink`,
+    headers:{
+      'authorization':'Basic YXBpOlRQUmg0RlpRWkhQTmpOUW5WTlhYWjNjSnh5eWJGVGgy',
+      'Content-type':'application/json'
+    },
+    data:req.body
+  }).then(result=>{
+    res.json({
+      status: 'ok',
+      data: result.data
+    })
+  })
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+module.exports = app;
