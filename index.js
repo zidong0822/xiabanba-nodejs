9fd20d98878751c57f534e5ad21c9dfcb3df18e6
lvwenhao <lvwenhao@huohua.cn>
2021-12-30 11:43:22 +0800

    api开发

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
-// Copyright 2017 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-'use strict';
-
-// [START gae_node_request_example]
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
+const express = require("express");
 const app = express();
 app.get('/', (req, res) => {
   res.send('hello world')
-})	
-app.use('/shrink', createProxyMiddleware({ target: 'https://api.tinify.com', changeOrigin: true,onProxyReq:(proxyReq)=>{
-    proxyReq.setHeader('authorization','Basic YXBpOlRQUmg0RlpRWkhQTmpOUW5WTlhYWjNjSnh5eWJGVGgy');
-    proxyReq.setHeader('Content-type','application/json');
-}}));
-app.listen(3000);
-// [END gae_node_request_example]
-
-module.exports = app;
+})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
