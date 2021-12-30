beb96ba6e9f720107d8ae0c492759503bcdf9d39
lvwenhao <lvwenhao@huohua.cn>
2021-12-30 11:54:40 +0800

    跨域设置

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const app = express();
+
+app.all("*",function(req,res,next){
+  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Headers","content-type");
+  res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
+  if (req.method.toLowerCase() == 'options')
+      res.send(200);
+  else
+      next();
+})
+
 app.get('/', (req, res) => {
   res.send('hello world')
 })
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
